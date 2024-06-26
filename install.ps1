4e64da8bb389bca69ea8fb13c5e646a4032d81d7
Csaba Sallai <68144659+csabika98@users.noreply.github.com>
2024-06-26 15:21:58 +0200

    NOTICKET Update install.ps1

--- install.ps1
+++ install.ps1
@@ -10,24 +10,26 @@ if ($currentPolicy -eq 'Restricted' -or $currentPolicy -eq 'AllSigned') {
     Write-Host "Current execution policy is $currentPolicy. The script will proceed."
 }
 
-
 if (!(Get-Command 'scoop' -ErrorAction SilentlyContinue)) {
     Write-Host "Installing Scoop..."
     Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 }
 
-
 scoop bucket add main
 scoop install php
 scoop install composer
 
-
 $username = [Environment]::UserName
 $phpIniDestPath = "C:\Users\$username\scoop\apps\php\current\"
-
 
 $scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $phpIniSourcePath = Join-Path -Path $scriptDirectory -ChildPath "php.ini"
 Copy-Item -Path $phpIniSourcePath -Destination $phpIniDestPath -Force
 
-Write-Host "PHP and Composer have been installed and configured."
+# New part: Copy database.sqlite to a new directory called 'database'
+$databaseSourcePath = Join-Path -Path $scriptDirectory -ChildPath "database.sqlite"
+$databaseDestDirectory = Join-Path -Path $scriptDirectory -ChildPath "database"
+New-Item -Path $databaseDestDirectory -ItemType Directory -Force
+Copy-Item -Path $databaseSourcePath -Destination $databaseDestDirectory -Force
+
+Write-Host "PHP and Composer have been installed and configured. Database file has been copied."
