c5feba5172f5986d0c1d4de36e67cac2d4170591
csabika98 <sallcsa.csaba8@gmail.com>
2024-07-03 23:49:23 +0200

    NOTICKET major changes in the PowerShell script

--- install.ps1
+++ install.ps1
@@ -1,16 +1,3 @@
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
-    Break
-}
-
-$currentPolicy = Get-ExecutionPolicy
-if ($currentPolicy -eq 'Restricted' -or $currentPolicy -eq 'AllSigned') {
-    Write-Warning "Current execution policy is $currentPolicy, which might prevent some scripts from running.`nConsider setting the policy to RemoteSigned or Bypass if you encounter issues."
-} else {
-    Write-Host "Current execution policy is $currentPolicy. The script will proceed."
-}
-
-
 if (!(Get-Command 'scoop' -ErrorAction SilentlyContinue)) {
     Write-Host "Installing Scoop..."
     Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
